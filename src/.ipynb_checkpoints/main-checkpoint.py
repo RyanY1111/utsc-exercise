281393a328037dc5e8acb2302d06e3b9dccff348
RyanY1111 <ryan.jang@mail.utoronto.ca>
2024-11-20 22:04:39 -0400

    Update main-checkpoint.py

--- src/.ipynb_checkpoints/main-checkpoint.py
+++ src/.ipynb_checkpoints/main-checkpoint.py
@@ -42,15 +42,30 @@ def get_avg_salary_by_job_title(employee_salary_df: pd.DataFrame, selected_title
 #TODO: MAKE THIS USING THE MODEL FUNCTION ABOVE (Copy pasting is your friend here)
 # HINT: The country column in the dataframe is 'Country'
 def get_avg_salary_by_country(employee_salary_df: pd.DataFrame, selected_countries: list):
-    pass
+    filtered_df = employee_salary_df[employee_salary_df['Country'].isin(selected_countries)]
+    
+    avg_salary_by_country = filtered_df.groupby('Country')['YearlyCompensation'].mean().reset_index()
+    
+    st.subheader("Average Salary by Country")
+    st.bar_chart(avg_salary_by_country, x='Country', y='YearlyCompensation')
 
 #TODO: MAKE THIS USING THE MODEL FUNCTION ABOVE (Copy pasting is your friend here)
 def get_num_employees_by_country(employee_salary_df: pd.DataFrame, selected_countries: list):
-    pass
+    filtered_df = employee_salary_df[employee_salary_df['Country'].isin(selected_countries)]
+
+    num_employees_by_country = filtered_df.groupby('Country')['EmployeeId'].count().reset_index()
+    
+    st.subheader("Number of Employees by Country")
+    st.bar_chart(num_employees_by_country, x='Country', y='EmployeeId')
 
 #TODO: MAKE THIS USING THE MODEL FUNCTION ABOVE (Copy pasting is your friend here)
 def get_num_employees_by_job_title(employee_salary_df: pd.DataFrame, selected_titles: list):
-    pass
+   filtered_df = employee_salary_df[employee_salary_df['JobTitle'].isin(selected_titles)]
+
+    num_employees_by_job_title = filtered_df.groupby('JobTitle')['EmployeeId'].count().reset_index()
+    
+    st.subheader("Number of Employees by Job Title")
+    st.bar_chart(num_employees_by_job_title, x='JobTitle', y='EmployeeId')
 
 if __name__ == '__main__':
     # Streamlit app title
